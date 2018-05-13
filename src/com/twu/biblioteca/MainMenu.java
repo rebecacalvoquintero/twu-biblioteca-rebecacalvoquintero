d6f4279e1201c319a9405cf835d0187151956974
Rebeca <rebecacalvoquintero@hotmail.es>
2018-05-13 14:49:32 +0100

    tested some logic from the biblioteca class

--- src/com/twu/biblioteca/MainMenu.java
+++ src/com/twu/biblioteca/MainMenu.java
@@ -1,7 +1,6 @@
 package com.twu.biblioteca;
 
 import java.util.Scanner;
-import java.util.List;
 
 
 public class MainMenu {
--- test/com/twu/biblioteca/BibliotecaAppTest.java
+++ test/com/twu/biblioteca/BibliotecaAppTest.java
@@ -1,5 +1,6 @@
 package com.twu.biblioteca;
 import com.twu.biblioteca.items.Book;
+import com.twu.biblioteca.items.Movie;
 
 import java.util.List;
 import java.util.stream.Collectors;
@@ -23,10 +24,47 @@ public class BibliotecaAppTest {
     public void getListOfBooksTest() {
         List <String> result = List.of("Eloquent Javascript", "Head First Java", "Programming Phoenix");
         List <Book> listOfBooks = Biblioteca.getListOfBooks();
-        List <String> listOfNames = listOfBooks.stream().map(Book -> Book.getName()).collect(Collectors.toList());
-        System.out.println("listOfNames");
-        System.out.println(listOfNames);
-        assertEquals(result, listOfNames);
+        List <String> listOfBookNames = listOfBooks.stream().map(Book -> Book.getName()).collect(Collectors.toList());
+        System.out.println("listOfBookNames");
+        System.out.println(listOfBookNames);
+        assertEquals(result, listOfBookNames);
     }
+
+    @Test
+    public void getListOfMovies() {
+        List <String> result = List.of("Grand Hotel Budapest", "Match Point", "Amelie");
+        List <Movie> listOfMovies = Biblioteca.getListOfMovies();
+        List <String> listOfMovieNames = listOfMovies.stream().map(Movie -> Movie.getName()).collect(Collectors.toList());
+        System.out.println("listOfMovieNames");
+        System.out.println(listOfMovieNames);
+        assertEquals(result, listOfMovieNames);
+    }
+
+    @Test
+    public void successfulLoginTest() {
+        Boolean result = true;
+        Boolean loginMethod = Biblioteca.login("777-7777", "4444");
+        assertEquals(result, loginMethod);
+    }
+
+    @Test
+    public void failLoginTest() {
+        Boolean result = false;
+        Boolean loginMethod = Biblioteca.login("777-7777", "3333");
+        assertEquals(result, loginMethod);
+    }
+
+    @Test
+    public void getUserDetailsTest() {
+        Boolean loggedUser = Biblioteca.login("777-7777", "4444");
+        String result = "User Informations:\n\n"+
+                "Name:         " + "Rebeca" +  "\n" +
+                "Email:        " + "rebecacalvoquintero@hotmail.es" + "\n" +
+                "Phone number: " + "+44 7593114428" + "\n";
+
+        assertEquals(result, Biblioteca.getUserLoggedDetails());
+    }
+
+
 
 }
